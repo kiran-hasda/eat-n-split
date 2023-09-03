9b665fb4dfc94b816c930f6d94a49417f15172ab
kiranweber <khasda885@gmail.com>
2023-09-03 11:43:03 +0530

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -81,6 +81,7 @@ export default function App() {
         <FormSpiltBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
